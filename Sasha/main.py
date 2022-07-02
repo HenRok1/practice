3772520b271d758e62a79b79f1be72a2bc45f47a
Mark0vAlexander <99321113+Mark0vAlexander@users.noreply.github.com>
2022-07-02 19:09:38 +0300

    Add files via upload

--- Sasha/main.py
+++ Sasha/main.py
@@ -1,5 +1,5 @@
-# name = str(input("Р’РІРµРґРёС‚Рµ РЅР°Р·РІР°РЅРёРµ С„Р°Р№Р»Р°: "))
-# base = int(input("Р’РІРµРґРёС‚Рµ РѕСЃРЅРѕРІР°РЅРёРµ РґРѕРєСѓРјРµРЅС‚Р°: "))
+# name = str(input("Введите название файла: "))
+# base = int(input("Введите основание документа: "))
 
 # print(name)
 
@@ -13,7 +13,7 @@
 # print(l)
 # f.close()
 # for i in range():
-#     command = int(input("Р’РІРµРґРёС‚Рµ РєРѕРјР°РЅРґСѓ: "))
+#     command = int(input("Введите команду: "))
 #     if command == 1:
 #         print(l[str(i)])
 #     else:
@@ -21,7 +21,7 @@
 # import pandas as pd
 #
 #
-# file = '/Users/aleksandrmarkov/Documents/РЈС‡РµР±Р°/РЎС‚СЂСѓРєС‚СѓСЂР°_Р±Рґ.xls'
+# file = '/Users/aleksandrmarkov/Documents/Учеба/Структура_бд.xls'
 #
 #
 # xl = pd.ExcelFile(file)
@@ -36,7 +36,7 @@ import os
 import shutil
 
 KEY_FOR_SEARCH = input('Что ищем?\n')
-PATH_FOR_COPY = input('Куда копировать файлы\n')
+PATH_FOR_COPY = input('Куда копировать файлы?\n')
 
 
 def search():
@@ -44,12 +44,12 @@ def search():
         if adress == PATH_FOR_COPY:
             continue
         for file in files:
-            if file.endswith('.docx'):
+            if file.endswith('.docx') or file.endswith('.doc'):
                 yield os.path.join(adress, file)
 
 
 def read_from_pathdocx(path):
-    with open(path) as r:
+    with open(path, encoding = "utf-16-le") as r:
         for i in r:
             if KEY_FOR_SEARCH in i:
                 return copy(path)
@@ -68,36 +68,36 @@ def copy(path):
             break
 
     shutil.copyfile(path, os.path.join(PATH_FOR_COPY, file_name))
-    print('Р¤Р°Р№Р» СЃРєРѕРїРёСЂРѕРІР°РЅ', file_name)
+    print('Файл скопирован', file_name)
 
 
 for i in search():
     try:
         read_from_pathdocx(i)
     except Exception as e:
-        with open(os.path.join(PATH_FOR_COPY, 'errors.docx'), 'a') as r:
+        with open(os.path.join(PATH_FOR_COPY, 'errors.txt'), 'a') as r:
             r.write(str(e) + '\n' + i + '\n')
 
 import csv
 import json
 
 # name_1 = "id"
-# name_2 = "Р¤.Р.Рћ."
-# name_3 = "РћСЃРЅРѕРІР°РЅРёРµ"
-# name_4 = "Р¤Р°Р№Р» в„–1"
-# name_5 = "Р¤Р°Р№Р» в„–2"
-# name_6 = "Р¤Р°Р№Р» в„–3"
-# name_7 = "Р¤Р°Р№Р» в„–4"
+# name_2 = "Ф.И.О."
+# name_3 = "Основание"
+# name_4 = "Файл №1"
+# name_5 = "Файл №2"
+# name_6 = "Файл №3"
+# name_7 = "Файл №4"
 
 # with open("practice.csv", "w") as file:
 #     practitioner = csv.practitioner(file, delimiter =",")
 #     practitioner.writerow(
 #         #[name_1, name_2, name_3, name_4, name_5, name_6, name_7]
-#         ("id", "Р¤.Р.Рћ.", "РћСЃРЅРѕРІР°РЅРёРµ", "Р¤Р°Р№Р» в„–1", "Р¤Р°Р№Р» в„–2", "Р¤Р°Р№Р» в„–3", "Р¤Р°Р№Р» в„–4")
+#         ("id", "Ф.И.О.", "Основание", "Файл №1", "Файл №2", "Файл №3", "Файл №4")
 #     )
 
 practitioner_data = [
-    ("id", "ФИО", "Основание", "Файл №1", "Файл №2", "Файл №3", "Файл №4")
+    ("id", "Ф.И.О.", "Основание", "Файл №1", "Файл №2", "Файл №3", "Файл №4")
 
 ]
 
@@ -109,7 +109,7 @@ practitioner_data = [
 #         )
 
 with open("practitioner.csv", "w") as file:
-    practitioner = csv.practitioner(file)
+    practitioner = csv.writer(file)
     practitioner.writerows(
         practitioner_data
     )
@@ -119,3 +119,8 @@ with open("practitioner.csv", "w") as file:
 #
 # asks = src[name_1, name_2, name_3, name_4, name_5, name_6, name_7]
 # for a in asks:
+
+
+#Практика
+#/Users/aleksandrmarkov/Desktop/Prog
+#/Users
