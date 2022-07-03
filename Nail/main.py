3c9d3d5ed110fe8e228a4ef30ce87edc51f9bda0
roberter <roberter@student.21-school.ru>
2022-07-03 16:53:02 +0300

    вроде работает

---
+++ Nail/main.py
@@ -0,0 +1,37 @@
+import openpyxl
+from docx import Document
+
+
+wordDoc = Document('main.docx')
+book = openpyxl.open("main.xlsx", read_only=True)
+sheet = book.active
+
+list_key = []
+
+for row in range(2, sheet.max_row + 1):
+    key = sheet[row][2].value
+
+    list_key.append(key)
+
+
+tables = wordDoc.tables # Получить таблицу, установленную в файле
+table = tables[0] # Получить первую таблицу в файле
+
+df = [[''for i in range(len(tables[0].columns))]for j in range(len(tables[0].rows))]
+
+for table in wordDoc.tables:
+    for i, row in enumerate(table.rows):
+        for j, cell in enumerate(row.cells):
+            df[i][j] = cell.text
+            for type_name in range(2, sheet.max_row + 1):
+                key = sheet[type_name][2].value
+                if key in cell.text:
+                        name = sheet[type_name][1].value
+                        cell.text = cell.text.replace(cell.text, name)
+                        print(cell.text)
+
+                # replace_val = input("Введите: " + name + " ")
+                # cell.text = cell.text.replace(cell.text, list_key)
+                # break
+
+wordDoc.save("example.docx")
