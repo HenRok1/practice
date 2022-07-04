44b337db17b31959821bd11961e9b3f73c11ea76
Mark0vAlexander <99321113+Mark0vAlexander@users.noreply.github.com>
2022-07-04 17:48:06 +0300

    Update main.py

--- Sasha/main.py
+++ Sasha/main.py
@@ -30,9 +30,14 @@ wordDoc4.save(path_save)
 
 
 
+id = input("Введите id:\n")
+doc_name = input("Введите название документа:\n")
+base = input("Введите основание:\n")
 practitioner_data = [
     ("id", "Документ", "Основание", "Файл №1", "Файл №2", "Файл №3", "Файл №4")
-
+]
+practitioner_data2 = [
+    (id, doc_name, base, path_file1, path_file2, path_file3, path_file4)
 ]
 
 
@@ -42,21 +47,8 @@ with open("practitioner.csv", "w", newline='', encoding='cp1251', errors="ignore
         practitioner_data
     )
 
-path_file = input("Введите название файла для импорта в таблицу:\n")
-with open(path_file) as csvfile:
-    result = json.load(csvfile)
-#print(result)
-
-id = input("Введите id\n")
-doc_name = input("Введите название документа:\n")
-base = input("Введите основание:\n")
-
-
-asks = result[id][doc_name][base][path_file1][path_file2][path_file3][path_file4]
-#print(asks)
-for a in asks:
-    with open("practitioner.csv", "a", newline='',  encoding='cp1251', errors="ignore") as csvfile:
-        practitioner = csv.writer(csvfile, delimiter=";")
-        practitioner.writerows(
-            asks
-        )
+with open("practitioner.csv", "a", newline='', encoding='cp1251', errors="ignore") as csvfile:
+    practitioner2 = csv.writer(csvfile, delimiter=";")
+    practitioner2.writerows(
+        practitioner_data2
+    )
