a4585d209301cc7e9f48f6fe552eb7626eefe1b6
Mark0vAlexander <99321113+Mark0vAlexander@users.noreply.github.com>
2022-07-04 14:53:49 +0300

    Update main.py

--- Sasha/main.py
+++ Sasha/main.py
@@ -1,8 +1,10 @@
 
 import os
+import shutil
 import json
 import csv
 from docx import Document
+
 
 file_name1 = input("Введите имя файла №1:\n")
 file_name2 = input("Введите имя файла №2:\n")
@@ -14,9 +16,11 @@ path_file1 = os.path.abspath(file_name1)
 path_file2 = os.path.abspath(file_name2)
 path_file3 = os.path.abspath(file_name3)
 path_file4 = os.path.abspath(file_name4)
-# wordDoc1 = Document(path_name1)
+
+
+Doc1 = shutil.copy(file_name1, path_save)
 wordDoc2 = Document(path_file2)
-# wordDoc3 = Document(path_name3)
+Doc3 = shutil.copy(file_name3, path_save)
 wordDoc4 = Document(path_file4)
 # wordDoc1.save(path_save)
 wordDoc2.save(path_save)
@@ -24,8 +28,9 @@ wordDoc2.save(path_save)
 wordDoc4.save(path_save)
 
 
+
 practitioner_data = [
-    ("id", "Имя файла", "Основание", "Файл №1", "Файл №2", "Файл №3", "Файл №4")
+    ("id", "Документ", "Основание", "Файл №1", "Файл №2", "Файл №3", "Файл №4")
 
 ]
 
@@ -41,7 +46,12 @@ with open(path_file) as csvfile:
     result = json.load(csvfile)
 #print(result)
 
-asks = result["id"]["Имя файла"]["Основание"][path_file1][path_file2][path_file3][path_file4]
+id = input("Введите id\n")
+doc_name = input("Введите название документа:\n")
+base = input("Введите основание:\n")
+
+
+asks = result[id][doc_name][base][path_file1][path_file2][path_file3][path_file4]
 #print(asks)
 for a in asks:
     with open("practitioner.csv", "a", newline='',  encoding='cp1251', errors="ignore") as csvfile:
