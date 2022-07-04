6d8881e4e24371163da01ce91d247bfbdb17d854
roberter <roberter@student.21-school.ru>
2022-07-04 14:56:10 +0300

    just

--- Liza/main.py
+++ Liza/main.py
@@ -1,11 +1,7 @@
 from docx import Document
 import pandas as pd
-import openpyxl as xl
-import re
-import docxtpl
-import numpy as np
 
-path=input("Полный путь к документу: ")
+path = input("Полный путь к документу: ")
 doc = Document(path)
 tables = doc.tables # Получить таблицу, установленную в файле
 table = tables [0] # Получить первую таблицу в файле
