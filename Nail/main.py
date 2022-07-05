ba90075b507b8dc8c6c4bf22989f22fd2174ca41
roberter <roberter@student.21-school.ru>
2022-07-05 23:33:25 +0300

    осталось сделать вставку таблиц

--- Nail/main.py
+++ Nail/main.py
@@ -2,32 +2,29 @@ import openpyxl
 from docx import Document
 
 
-wordDoc = Document('main.docx')
-book = openpyxl.open("../Liza/main.xlsx", read_only=True)
-sheet = book.active
-
-list_key = []
-
-for row in range(2, sheet.max_row + 1):
-    key = sheet[row][2].value
-
-    list_key.append(key)
+def replace_word(sheet):
+    for type_name in range(2, sheet.max_row + 1):
+        table_key = sheet[type_name][2].value
+        if table_key in cell.text:
+            table_name = sheet[type_name][1].value
+            quest = input("Введите " + table_name + ": ")
+            cell.text = cell.text.replace(table_key, quest)
 
 
-tables = wordDoc.tables # Получить таблицу, установленную в файле
-table = tables[0] # Получить первую таблицу в файле
+path_1 = input("Введите путь к файлу .docx: ")
+path_2 = input("Введите путь к файлу .xlsx: ")
 
-df = [[''for i in range(len(tables[0].columns))]for j in range(len(tables[0].rows))]
+wordDoc = Document(path_1)
+book = openpyxl.open(path_2, read_only=True)
+sheet = book.worksheets[0]
+
+tables = wordDoc.tables
+table = tables[0]
 
 for table in wordDoc.tables:
     for i, row in enumerate(table.rows):
         for j, cell in enumerate(row.cells):
-            df[i][j] = cell.text
-            for type_name in range(2, sheet.max_row + 1):
-                key = sheet[type_name][2].value
-                if key in cell.text:
-                        name = sheet[type_name][1].value
-                        quest = input("Введите " + name + " ")
-                        cell.text = cell.text.replace(key, quest)
+            replace_word(sheet)
+
 
 wordDoc.save("example.docx")
